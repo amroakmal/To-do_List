3abbd624f565ac504051c5b30c3af7f2943f57e7
AmrAkmal <amrakmal97@gmail.com>
2020-04-30 08:00:54 +0200

    Solve multiple item insertion

--- app.js
+++ app.js
@@ -33,30 +33,33 @@ const def_item3 = new Item({
 
 const def_items = [def_item1, def_item2, def_item3];
 
-Item.insertMany(def_items, function(err) {
-  if(err) {
-    console.log("Error occured!");
-  }
-});
-
 
 app.get("/", function(req, res) {
-
-  res.render("list", {listTitle: "Today", newListItems: items});
-
+  Item.find(function(err, foundItems) {
+    if(err) {
+      console.log("Error occured!");
+    }
+    else {
+      if(foundItems.length === 0) {
+          Item.insertMany(def_items, function(err) {
+            if(err) {
+              console.log("Error occured!");
+            }
+          });
+          res.redirect("/");
+        }
+        else {
+          res.render("list", {listTitle: "Today", newListItems: foundItems});      
+        }
+    }
+  });
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  
+  
 });
 
 app.get("/work", function(req,res){
